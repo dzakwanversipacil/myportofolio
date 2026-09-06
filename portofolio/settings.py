5bfe2723086880fba75152474bfff06bfea7e943
dzakwanversipacil <dzakwan.farabi@ui.ac.id>
2026-09-06 15:33:54 +0700

    feat: Initial Experience Section (Plain)

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wi)#a-72s_@89qd4kp-jwmnn!zcj&fmv$y%ve52j7&rp7$hocv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
