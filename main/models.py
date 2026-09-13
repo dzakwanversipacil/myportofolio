dee3937eb3dd46ef870c13790535877364f9b726
dzakwanversipacil <dzakwan.farabi@ui.ac.id>
2026-09-14 02:02:08 +0700

    chore(model): delete unused 'title' field from Experience model

--- main/models.py
+++ main/models.py
@@ -46,7 +46,6 @@ class Experience(models.Model):
 
 class Education(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
     description = models.TextField()
     organization = models.CharField(max_length=255, default='')
     field_of_study = models.CharField(max_length=255, default='')
@@ -55,7 +54,7 @@ class Education(models.Model):
     image_url = models.URLField(max_length=500, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.organization
         
     @property
     def is_ongoing(self):
